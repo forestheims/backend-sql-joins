f57feafe95908552bb9ab1afa07e7ee84ca0ce68
forestheims <forestheims@gmail.com>
2022-03-21 12:30:27 -0700

    exercise 5 complete

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,6 +1,6 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
-SELECT first_name, last_name, address.address, city.city
+SELECT first_name, last_name, address, city
 FROM customer
 LEFT JOIN address
 ON customer.address_id = address.address_id
